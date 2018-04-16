ed3770322ab30b8a61c6ec5244ce9a857b95805a
百道ひかる <hyakudohikaru@ld10-hikaru-hyakudo.local>
2018-04-16 20:38:50 +0900

    企画の簡単なコードを送りました。

---
+++ myProject/src/ofApp.cpp
@@ -0,0 +1,99 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    ofSetFrameRate(60);
+    ofSetCircleResolution(60);
+    
+    //変数の初期
+    variable();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    Circle();
+    
+    //落とす作業
+    speed = speed + accele;
+    circleCenter.y = circleCenter.y +speed;
+
+}
+
+//--------------------------------------------------------------
+void ofApp::variable(){
+    radian =100;
+    Center = ofVec2f(ofGetWidth()/2,ofGetHeight()/2);
+    circleCenter = ofVec2f(Center.x,-100);
+    speed = 1;
+    accele = 1;
+}
+
+//円------------------------------------------------------------
+void ofApp::Circle(){
+    for (int i = 0; i<12; i++) {
+        ofSetColor(255);
+        ofDrawCircle(circleCenter.x-(radian*sin(PI/6*i)),circleCenter.y-(radian*cos(PI/6*i)), 2);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
